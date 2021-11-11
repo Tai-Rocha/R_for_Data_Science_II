ea2509b0e69ab99ea9f3702a7703e2b9cafb4d6f
Tai-Rocha <taina013@gmail.com>
2021-11-11 14:21:18 -0300

    add scritps day 1 and 2

---
+++ R/Day_1/0_create_project_from_usethis.R
@@ -0,0 +1,9 @@
+######################################
+## Create a project from use_this pckg
+######################################
+
+### Run this command in None project. After run this command the R opens new section (the project)
+
+usethis::create_project("home/tai-rocha/Documents/Github/R_for_Data_Science_II")
+
+
