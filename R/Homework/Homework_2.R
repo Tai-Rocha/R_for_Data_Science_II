3f38ec831af1ae61f9b4f608ef993a9b955f0e5e
Tai-Rocha <taina013@gmail.com>
2021-11-11 22:22:47 -0300

    work in progress...

--- R/Homework/Homework_2.R
+++ R/Homework/Homework_2.R
@@ -1,19 +1,20 @@
-################
-## Practice
-################
+################################################################################
+## Practice: Rewrite Way 1 codes with across() e where functions of dplyR pckgs
+## across()
+## where() is for test, usually with "is"
+## R version 4.1.2
+## Use " Enter" all times you open a parathesis
+################################################################################
+
+library(dplyr)
+
+casas <- readRDS("data/casas.rds")
 
 # a
-dados::casas
 
-## Way 1
-casas |>
-  group_by(geral_qualidade) |>
-  summarise(across(
-    .cols = c(acima_solo_area, garagem_area, venda_valor), # Variables
-    .fns = mean, na.rm = T ))
+  ## Way 1
 
-## Way 2
-casas |>
+  casas |>
   group_by(geral_qualidade) |>
   summarise(
     acima_solo_area_media = mean(acima_solo_area, na.rm = TRUE),
@@ -21,32 +22,43 @@ casas |>
     valor_venda_medio = mean(venda_valor, na.rm = TRUE)
   )
 
+  ## Way 2
+
+  casas |>
+  group_by(geral_qualidade) |>
+  summarise(across(
+    .cols = c(acima_solo_area, garagem_area, venda_valor), # Variables
+    .fns = mean, na.rm = T ))
+
 
 # b)
 
-# way 1
-
-casas|>
-      summarise(across
-                (.col= (where(c(porao_qualidade, varanda_fechada_area, cerca_qualidade),
-                 ~!is.na(.x)))))
-
-# way 2
-
-casas |>
+  ## Way 1
+  casas |>
   filter_at(
     vars(porao_qualidade, varanda_fechada_area, cerca_qualidade),
     ~!is.na(.x)
-  )
+    )
 
+
+  ## Way 2
+
+  casas|>
+  summarise(across(
+            .cols = c(porao_qualidade, varanda_fechada_area,   cerca_qualidade),
+            .fns = ~!is.na(.x)
+            )
+  )
 
 # c
 
-# Way 1
-casas |>
-  summarise(across(where(is.character)), ~tidyr::replace_na(.x, replace = "Não possui"))
+  ## Way 1
 
-
-# Way 2
-casas |>
+  casas |>
   mutate_if(is.character, ~tidyr::replace_na(.x, replace = "Não possui"))
+
+
+  ## Way 2 (not work)
+
+  casas |>
+  summarise(across(.cols = where(is.character)), .fns = ~tidyr::replace_na(.x, replace = "Não possui"))
--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ### [Google classroom](https://classroom.google.com/u/0/c/MzczNjg1MjM2NTk5)
 
 ### [Webpage course](https://curso-r.github.io/202111-r4ds-2/)
+
+
+?stringi::`stringi-search-regex` 
